86d11dfb99b17f2e3957bafb35be020ea1a42424
sumtuckyi <113111711+sumtuckyi@users.noreply.github.com>
2024-02-07 22:44:47 +0900

    Update settings.py

--- ssafy_pjt_django/settings.py
+++ ssafy_pjt_django/settings.py
@@ -184,6 +184,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
